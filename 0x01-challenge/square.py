9a47c0f0a640e91abc5815d0ac67acd7ada1bdf1
odelolajosh <oladapoodelola@gmail.com>
2022-10-11 23:47:05 +0100

    chore: lint PEP8

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" Square """
+
 
 class square():
-    
+    """Square"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Initialize a square """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +17,15 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.width * 2)
+        """ Perimeter of square """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String rep """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
