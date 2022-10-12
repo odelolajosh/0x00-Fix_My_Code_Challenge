42aa8f862799ae4162bf3c0f22e65933fe816a64
odelolajosh <oladapoodelola@gmail.com>
2022-10-12 01:30:52 +0100

    fix: fixed square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         """ Documentation """
         return (self.width * 2) + (self.height * 2)
 
@@ -27,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())i
+    print(s.perimeter_of_my_square())
