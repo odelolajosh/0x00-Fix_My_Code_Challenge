cf9197a61c0f756d9bc5e5dc16fb969261098c09
odelolajosh <oladapoodelola@gmail.com>
2022-10-12 01:32:54 +0100

    fix: fixed square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -14,7 +14,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
         """ Documentation """
