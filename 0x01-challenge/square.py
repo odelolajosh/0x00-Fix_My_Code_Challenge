f10f855c986dfada2687daddf927591394276b71
odelolajosh <oladapoodelola@gmail.com>
2022-10-12 01:23:42 +0100

    fix: fixed square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 class square():
-    
+    """ Documentation """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Documentation """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +16,13 @@ class square():
         return self.width * self.height
 
     def perimeter_of_my_square(self):
+        """ Documentation """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Documentation """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
