776aa49f8d337b2f2fc7124a20a2df7e0abf97f1
odelolajosh <oladapoodelola@gmail.com>
2022-10-12 01:31:27 +0100

    fix: fixed square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Documentation """
 
 
-class square():
+class Square():
     """ Documentation """
     width = 0
     height = 0
@@ -27,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
